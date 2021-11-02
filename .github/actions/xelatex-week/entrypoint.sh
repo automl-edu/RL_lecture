2f144e2060f7914b20df7d231466a708082a88b6
dwoiwode <dominik.woiwode@stud.uni-hannover.de>
2021-11-02 14:30:30 +0100

    Fix typo

--- .github/actions/xelatex-week/entrypoint.sh
+++ .github/actions/xelatex-week/entrypoint.sh
@@ -4,6 +4,4 @@
 # https://docs.github.com/en/actions/creating-actions/dockerfile-support-for-github-actions#example-entrypointsh-file
 
 cd $1
-ls -l
-sh -c "for f in *.tex ; do echo $f ; done"
 sh -c "for f in *.tex ; do xelatex -interaction=nonstopmode $f ; done"
