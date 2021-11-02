4e7a04f0713a964cd2606e498831e0076a19db97
dwoiwode <dominik.woiwode@stud.uni-hannover.de>
2021-11-02 12:38:59 +0100

    Change directory for entrypoint

--- .github/actions/xelatex/entrypoint.sh
+++ .github/actions/xelatex/entrypoint.sh
@@ -3,4 +3,5 @@
 # Recommended entrypoint script:
 # https://docs.github.com/en/actions/creating-actions/dockerfile-support-for-github-actions#example-entrypointsh-file
 
-sh -c "xelatex -interaction=nonstopmode -include-directory=$1 $1/$2"
+cd $1
+sh -c "xelatex -interaction=nonstopmode $2"
