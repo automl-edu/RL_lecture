46bd4ef72642bd69ed66bf299deed434a367d503
dwoiwode <dominik.woiwode@stud.uni-hannover.de>
2021-11-02 13:18:15 +0100

    Use sh -c

--- .github/actions/xelatex-week/entrypoint.sh
+++ .github/actions/xelatex-week/entrypoint.sh
@@ -4,6 +4,4 @@
 # https://docs.github.com/en/actions/creating-actions/dockerfile-support-for-github-actions#example-entrypointsh-file
 
 cd $1
-for f in *.tex; do
-  sh -c "xelatex -interaction=nonstopmode $f"
-done
+sh -c "for f in *.tex ; do xelatex -interaction=nonstopmode $f ; done"
