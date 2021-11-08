6c3b2d408cdeed84df7611ac3f0b4e449343288c
dwoiwode <dominik.woiwode@stud.uni-hannover.de>
2021-11-08 22:09:27 +0100

    Update for handout-mode

--- .github/actions/xelatex-week/entrypoint.sh
+++ .github/actions/xelatex-week/entrypoint.sh
@@ -4,6 +4,8 @@
 # https://docs.github.com/en/actions/creating-actions/dockerfile-support-for-github-actions#example-entrypointsh-file
 
 cd $1
+sh -c "mkdir $2";
 for f in *.tex ; do
-  sh -c "xelatex -interaction=nonstopmode $f" ;
+  sh -c "xelatex -interaction=nonstopmode -jobname=${f%.*} \"\def\is$2 \input{$f}\"" ;
+  sh -c "mv ${f%.*}.pdf handout/${f%.*}.pdf"
 done
--- .github/actions/xelatex/entrypoint.sh
+++ .github/actions/xelatex/entrypoint.sh
@@ -4,4 +4,8 @@
 # https://docs.github.com/en/actions/creating-actions/dockerfile-support-for-github-actions#example-entrypointsh-file
 
 cd $1
-sh -c "xelatex -interaction=nonstopmode $2"
+if [$3 -eq "handout"]; then
+    sh -c "xelatex -interaction=nonstopmode -jobname=${$2.*}-handout \"\def\ishandout \input{$2}\"" ;
+  else
+    sh -c "xelatex -interaction=nonstopmode $2" ;
+  fi;
